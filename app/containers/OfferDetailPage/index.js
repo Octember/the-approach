5946c9c5a7441f383f2301284b757e2c7fc37625
Noah Lindner <noahlindner@curalate.com>
2018-01-01 15:23:51 -0800

    remove show more

--- app/containers/OfferDetailPage/index.js
+++ app/containers/OfferDetailPage/index.js
@@ -17,7 +17,7 @@ import makeSelectOfferDetailPage from './selectors';
 import reducer from './reducer';
 import saga from './saga';
 
-import ShowMore from 'react-show-more';
+// import ShowMore from 'react-show-more';
 import { DateRangePicker, SingleDatePicker, DayPickerRangeController } from 'react-dates';
 
 
@@ -84,9 +84,9 @@ export class OfferDetailPage extends React.Component { // eslint-disable-line re
 
           <PageSection title="Itinerary">
             <BorderBottomDiv className="pb-2">
-              <ShowMore>
-                3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-              </ShowMore>
+              {/*<ShowMore>*/}
+                {/*3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.*/}
+              {/*</ShowMore>*/}
             </BorderBottomDiv>
           </PageSection>
 
