d94f7af8f8508daf935aa7013f91ca68d4c987ac
Ezra Schwartz <ezraschwartz@mercury.local>
2018-01-02 22:17:03 -0500

    Changed Header so that the nav bar functions as a tool to navigate available pages

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -14,7 +14,7 @@ const StyledNavButton = styled.button `
   border-color: rgba(0,0,0,0) !important; 
 `;
 
-function Header() {
+function fakeHeader() {
   return (
     <div className="row">
       <nav className="navbar navbar-toggleable-xs navbar-light bg-fafafa pb-0 w-100">
@@ -25,6 +25,36 @@ function Header() {
         <Link className="navbar-brand mr-auto ml-2" to="/"> theApproach</Link>
       </nav>
     </div>
+  );
+}
+
+function Header() {
+  return(
+    <div className="pos-f-t">
+    <div className="collapse" id="navbarToggleExternalContent">
+      <div className="bg-inverse text-white">
+        <Link className="text-muted" to ="/">Home</Link>
+        <br>
+        </br>
+        <Link className="text-muted" to ="locationlist">Location List </Link>
+        <br>
+        </br>
+        <Link className="text-muted" to ="location/:1">Location 1 </Link>
+        <br>
+        </br>
+        <Link className="text-muted" to ="offerlist">Offerlist</Link>
+        <br>
+        </br>
+        <Link className="text-muted" to ="form">Submit Review </Link>
+      </div>
+    </div>
+    <nav className="navbar navbar-toggleable-xs navbar-light bg-fafafa pb-0 w-100">
+      <StyledNavButton className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </StyledNavButton>
+      <Link className="navbar-brand mr-auto ml-2" to="/"> theApproach</Link>
+    </nav>
+  </div>
   );
 }
 
