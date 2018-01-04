2b8c2ad2934fe3f3541a519200417ccfbd02c912
Ezra Schwartz <ezraschwartz42@gmail.com>
2018-01-03 20:25:14 -0500

    NavBar in Header gives dropdown of selected pages (#10)
    * Changed Header so that the nav bar functions as a tool to navigate available pages
    
    * Made nav classes into nav-item and removed breaks
    
    * Changed a and href to Link and to

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -15,18 +15,37 @@ const StyledNavButton = styled.button `
 `;
 
 function Header() {
-  return (
-    <div className="row">
-      <nav className="navbar navbar-toggleable-xs navbar-light bg-fafafa pb-0 w-100">
-
-        <StyledNavButton className="navbar-toggler navbar-toggler-right px-0" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon" />
-        </StyledNavButton>
-        <Link className="navbar-brand mr-auto ml-2" to="/"> theApproach</Link>
-      </nav>
+  return(
+    <div className="pos-f-t">
+    <div className="collapse" id="navbarToggleExternalContent">
+      <div className="bg-inverse text-white">
+       <ul className="nav flex-column">
+          <li className ="nav-item">
+            <Link className ="nav-link active" to='/'>Home</Link>
+          </li>
+          <li className ="nav-item">
+            <Link className ="nav-link active" to='locationlist'>Location List</Link>
+          </li>
+          <li className ="nav-item">
+            <Link className ="nav-link active" to='offerlist'>Offer List</Link>
+          </li>
+          <li className ="nav-item">
+            <Link className ="nav-link active" to='form'>Submit Review</Link>
+          </li>
+      </ul>
+        
+      </div>
     </div>
+    <nav className="navbar navbar-toggleable-xs navbar-light bg-fafafa pb-0 w-100">
+      <StyledNavButton className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </StyledNavButton>
+      <Link className="navbar-brand mr-auto ml-2" to="/"> theApproach</Link>
+    </nav>
+  </div>
   );
 }
+
 
 Header.propTypes = {};
 
