48bb69fe138be736e62309248b369799fb7cb625
Noah Lindner <noahlindner@curalate.com>
2018-01-06 14:58:19 -0800

    Reviews on location page, without images yet

--- app/components/PageSection/index.js
+++ app/components/PageSection/index.js
@@ -8,13 +8,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function PageSection(props) {
+  const title = props.title ? <h4 className="font-weight-bold">{props.title}</h4> : '';
   return (
     <div className="container px-0">
       <div className="row py-2">
         <div className="col" />
 
         <div className="col-md-8">
-          <h4 className="font-weight-bold">{props.title}</h4>
+          {title}
           {props.children}
         </div>
 
