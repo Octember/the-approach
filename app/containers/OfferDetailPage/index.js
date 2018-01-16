427e0ed3eaf2d9142915ba8505016bb92f4e5a05
Noah Lindner <noahlindner@curalate.com>
2018-01-15 17:02:35 -0800

    Fix offer detail page

--- app/containers/OfferDetailPage/index.js
+++ app/containers/OfferDetailPage/index.js
@@ -17,13 +17,8 @@ import makeSelectOfferDetailPage from './selectors';
 import reducer from './reducer';
 import saga from './saga';
 
-// import ShowMore from 'react-show-more';
-import { DateRangePicker, SingleDatePicker, DayPickerRangeController } from 'react-dates';
-
-
 import Header from 'components/Header'
 import Breadcrumbs from 'components/Breadcrumbs'
-// import LocationTitle from 'components/LocationTitle';
 import GuideHero from 'components/GuideHero';
 import ScheduleTripBox from 'components/ScheduleTripBox';
 import PageSection from "../../components/PageSection";
@@ -55,58 +50,38 @@ export class OfferDetailPage extends React.Component { // eslint-disable-line re
         </Helmet>
 
         <div className="container">
+          <Header />
+
           <div className="row">
-            <div className="col" />
-            <div className="col-md-8 px-0">
-              <div className="container">
-                <Header />
-                <Breadcrumbs
-                  breadcrumbData={[
-                    { link: 'google.com', text: 'Mt. Rainier National park' },
-                    { link: 'google.com', text: 'chicken butt' },
-                  ]}
-                />
-                {/*<LocationTitle className="mt-0 mb-2" title="Eamon-Winthrop Glacier" rating={5.0} />*/}
-              </div>
-              <BorderBottomDiv>
-                <GuideHero />
-              </BorderBottomDiv>
-            </div>
-            <div className="col" />
-          </div>
+            <div className="col-lg-7 px-0">
 
-          <PageSection>
+
             <BorderBottomDiv>
-              <ScheduleTripBox {...offer} />
+              <GuideHero />
             </BorderBottomDiv>
-          </PageSection>
 
-          <PageSection title="Itinerary">
-            <BorderBottomDiv className="pb-2">
-              {/*<ShowMore>*/}
-                {/*3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.*/}
-              {/*</ShowMore>*/}
-            </BorderBottomDiv>
-          </PageSection>
+            <PageSection>
+              <BorderBottomDiv>
+                <ScheduleTripBox {...offer} />
+              </BorderBottomDiv>
+            </PageSection>
 
-          <PageSection>
-            <DayPickerRangeController
-              startDate={this.state.startDate} // momentPropTypes.momentObj or null,
-              endDate={this.state.endDate} // momentPropTypes.momentObj or null,
-              onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })} // PropTypes.func.isRequired,
-              focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
-              onFocusChange={focusedInput => this.setState({ focusedInput })} // PropTypes.func.isRequired,
-              isDayHighlighted={moment => moment.day() == 4}
-            />
-          </PageSection>
+            <PageSection title="Itinerary">
+              <BorderBottomDiv className="pb-2">
+                {/*<ShowMore>*/}
+                  3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                {/*</ShowMore>*/}
+              </BorderBottomDiv>
+            </PageSection>
 
-          <PageSection title="Guides">
-            <GuideCard />
-            <GuideCard />
-            <GuideCard />
+            <PageSection title="Guides">
+              <GuideCard />
+              <GuideCard />
+              <GuideCard />
 
-          </PageSection>
-
+            </PageSection>
+            </div>
+          </div>
         </div>
       </div>
     );
