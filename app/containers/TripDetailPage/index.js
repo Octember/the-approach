6c6f3dba14780786b306cf9a474f22fadd2cdde3
Ezra Schwartz <ezraschwartz@reed.edu>
2018-01-25 04:32:22 -0800

    Added Guide Information card and removed bottom div from desktop schedule box

--- app/containers/TripDetailPage/index.js
+++ app/containers/TripDetailPage/index.js
@@ -21,6 +21,7 @@ import { TRIP_DOMAIN } from './constants'
 
 import Header from 'components/Header'
 import GuideHero from 'components/GuideHero';
+import GuideDescriptionCard from 'components/GuideDescriptionCard';
 import ScheduleTripBox from 'components/ScheduleTripBox';
 import { LocationCarouselDesktop, LocationCarouselMobile } from 'components/LocationCarousel';
 import OfferScheduleBox from 'components/OfferScheduleBox';
@@ -63,25 +64,28 @@ export class TripDetailPage extends React.Component { // eslint-disable-line rea
                 {/* Mobile/tablet view */}
                 <LocationCarouselMobile title={'Crevasse Rescue Course'} metadata={metadata} images={this.props.locationData.images} rating={5} />
 
-                <BorderBottomDiv>
-                  <ScheduleTripBox guide={this.props.guide} />
-                </BorderBottomDiv>
               </div>
 
-              <PageSection title="Description">
+              <PageSection title="Guides">
                 <BorderBottomDiv className="pb-2">
-                  {this.props.trip.heading}
+                  <GuideDescriptionCard className="ml-3" guideName='RMI Expeditions'/>
                 </BorderBottomDiv>
               </PageSection>
 
-            
-              
-            
+              <PageSection title="Description">
+                <BorderBottomDiv className="pb-2">
+                  <div className="ml-3">
+                    {this.props.trip.heading}
+                  </div>
+                </BorderBottomDiv>
+              </PageSection>
 
               <PageSection title="Itinerary">
                 <BorderBottomDiv className="pb-2">
                   {/*<ShowMore>*/}
-                    3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                    <div className="ml-3">
+                      3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                    </div>
                   {/*</ShowMore>*/}
                 </BorderBottomDiv>
               </PageSection>
@@ -108,9 +112,7 @@ export class TripDetailPage extends React.Component { // eslint-disable-line rea
                 images={this.props.locationData.images}
                 desktopView={true}
               />
-              <BorderBottomDiv className="pb-2">
-                <OfferScheduleBox className="m-2"/>
-              </BorderBottomDiv> 
+              <OfferScheduleBox className="m-2"/>
             </div>
 
           </div>
