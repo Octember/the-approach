c256a909b0af71722ff9de4c4c072993e43d46c8
Noah Lindner <noahlindner@curalate.com>
2018-02-03 15:38:20 -0800

    Basic validation

--- app/components/PageSection/index.js
+++ app/components/PageSection/index.js
@@ -9,20 +9,22 @@ import PropTypes from 'prop-types';
 
 function PageSection(props) {
   const title = props.title ? <h4 className="font-weight-bold px-2">{props.title}</h4> : '';
+
+  const className = props.className ? `py-2 ${props.className}` : 'py-2';
   return (
-    <div className="py-2">
+    <div className={className}>
       {title}
       <div>
         {props.children}
       </div>
     </div>
-
   );
 }
 
 PageSection.propTypes = {
   title: PropTypes.string,
   children: PropTypes.node,
+  className: PropTypes.string
 };
 
 export default PageSection;
