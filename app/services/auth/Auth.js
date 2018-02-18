74ef262240aa5dc0e14fd9d7be459bfdd561bd3c
Noah Lindner <noahlindner@curalate.com>
2018-02-18 11:11:47 -0800

    PR feedback

--- app/services/auth/Auth.js
+++ app/services/auth/Auth.js
@@ -4,32 +4,27 @@ import history from './history';
 
 export default class Auth {
   constructor() {
-    this.login = this.login.bind(this);
-    this.logout = this.logout.bind(this);
-    this.handleAuthentication = this.handleAuthentication.bind(this);
-    this.isAuthenticated = this.isAuthenticated.bind(this);
-  }
-
-  lock = new Auth0Lock(AUTH_CONFIG.clientId, AUTH_CONFIG.domain, {
-    autoclose: true,
-    auth: {
-      redirectUrl: AUTH_CONFIG.callbackUrl,
-      responseType: 'token id_token',
-      audience: `https://${AUTH_CONFIG.domain}/userinfo`,
-      params: {
-        scope: 'openid',
+    this.lock = new Auth0Lock(AUTH_CONFIG.clientId, AUTH_CONFIG.domain, {
+      autoclose: true,
+      auth: {
+        redirectUrl: AUTH_CONFIG.callbackUrl,
+        responseType: 'token id_token',
+        audience: `https://${AUTH_CONFIG.domain}/userinfo`,
+        params: {
+          scope: 'openid',
+        },
       },
-    },
-    languageDictionary: {
-      title: 'The Approach',
-    },
-  });
-
-  login() {
-    this.lock.show();
+      languageDictionary: {
+        title: 'The Approach',
+      },
+    });
   }
 
-  handleAuthentication() {
+  login = () => {
+    this.lock.show();
+  };
+
+  handleAuthentication = () => {
     // Add a callback for Lock's `authenticated` event
     this.lock.on('authenticated', this.setSession.bind(this));
     // Add a callback for Lock's `authorization_error` event
@@ -38,7 +33,7 @@ export default class Auth {
       alert(`Error: ${err.error}. Check the console for further details.`);
       history.goBack();
     });
-  }
+  };
 
   setSession(authResult) {
     if (authResult && authResult.accessToken && authResult.idToken) {
@@ -52,19 +47,19 @@ export default class Auth {
     }
   }
 
-  logout() {
+  logout = () => {
     // Clear access token and ID token from local storage
     localStorage.removeItem('access_token');
     localStorage.removeItem('id_token');
     localStorage.removeItem('expires_at');
     // Reload current page
     history.go(history.location);
-  }
+  };
 
-  isAuthenticated() {
+  isAuthenticated = () => {
     // Check whether the current time is past the
     // access token's expiry time
     const expiresAt = JSON.parse(localStorage.getItem('expires_at'));
     return new Date().getTime() < expiresAt;
-  }
+  };
 }
